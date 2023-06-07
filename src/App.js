34acedaae35584b0deb77b5c32beddb59304c619
sohe89 <soniaherreradg@gmail.com>
2023-06-07 20:32:54 -0300

    07062023

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@
 //import Minorista from "./Minorista";
 //import Contacto from "./Contacto"
 //import Navbar from "./Navbar";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./Footer";
 import {Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 /*Nuevo elementos*/
 import HomeStore from "./component/HomeStore";
 import NavbarStore from "./component/NavbarStore";
 import Products from "./component/Products";
 import Product from "./component/Product";
+import CartStore from "./component/CartStore";
 
 
 function App() {
@@ -20,8 +24,9 @@ function App() {
         <Route  path="/" element={<HomeStore/>} />
         <Route path="/products" element={<Products/>}/>
         <Route path="/products/:id" element={<Product/>}/>
+        <Route path="/cart" element={<CartStore/>}/>
 </Routes>
-
+<ToastContainer/>
 <Footer/>
 </>
 
